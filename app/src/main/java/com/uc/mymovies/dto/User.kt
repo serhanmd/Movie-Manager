e0339c335bb0bcab741a31e29e8e895887044514
serhanmd <70541399+serhanmd@users.noreply.github.com>
2021-03-02 15:55:23 -0500

    Revert "Database stuff ck"

--- app/src/main/java/com/uc/mymovies/dto/User.kt
+++
@@ -1,4 +0,0 @@
-package com.uc.mymovies.dto
-
-data class User(val id: Int, val username: String, val password: String, val profileName: String, val email: String ) {
-}
--- app/src/test/java/com/uc/mymovies/ExampleUnitTest.kt
+++ app/src/test/java/com/uc/mymovies/ExampleUnitTest.kt
@@ -1,9 +1,5 @@
 package com.uc.mymovies
 
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreSettings
-import com.uc.mymovies.dto.Movie
-import com.uc.mymovies.service.MovieService
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -14,30 +10,8 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-
-    private lateinit var firestore : FirebaseFirestore
-
-    init {
-        firestore = FirebaseFirestore.getInstance()
-        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
-    }
-
-
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-
-    @Test
-    fun canRetrieveFromFirebase() {
-//        var movieService: MovieService = MovieService()
-//        movieService.fetchUserMovies("connorkeith")
-
-        var movies = firestore?.collection("users").document("connorkeithck")
-        movies.get().addOnSuccessListener { document ->
-            if (document != null)
-                print(document)
-        }
-    }
-
 }
