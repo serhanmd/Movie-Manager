58aedf4896295f4d89294a436945fe19cb141769
7amood <mohamedserhan7@gmail.com>
2021-04-03 18:24:01 -0400

    Make changes to MainActivity to use MainFragment and MainViewModel

--- app/src/main/java/com/uc/mymovies/MainActivity.kt
+++ app/src/main/java/com/uc/mymovies/MainActivity.kt
@@ -2,28 +2,16 @@ package com.uc.mymovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.ListAdapter
-import android.widget.ListView
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreSettings
-import com.uc.mymovies.service.MovieService
 
 class MainActivity : AppCompatActivity() {
-
-    var movieService: MovieService = MovieService()
-
-    init {
-        fetchMovies()
-    }
-
-    private fun fetchMovies() {
-       var result = movieService.fetchUserMovies("connorkeith")
-        Log.d("TEST-LOG: ", result.toString())
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
     }
 }
